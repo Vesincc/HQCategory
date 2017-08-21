0329387775a406edadf11611aa1ee78cb7a3bded
韩琪 <HanQi@hanqideMacBook-Pro.local>
2017-08-21 10:38:27 +0800

    init

---
+++ HQCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQCategory
+//
+//  Created by HanQi on 2017/8/21.
+//  Copyright © 2017年 HanQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HQCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQCategory
+//
+//  Created by HanQi on 2017/8/21.
+//  Copyright © 2017年 HanQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
